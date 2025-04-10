e1c71287aa84da67ba578d3e79af5591c9588e33
M. Toups <mtoups3@tulane.edu>
2025-04-10 15:16:54 -0500

    turn off VNC/etc which will hopefully help this start up on cloudlab (won't require IP address, etc)

--- profile.py
+++ profile.py
@@ -1,8 +1,9 @@
 """This is a basic profile with two hosts connected by a router.
 Instructions:
-Wait for the profile instance to start, then log in to each node by clicking on it in the topology and choosing the `shell` menu item. 
+Wait for the profile instance to start, then log in to each node by clicking on it in the topology and choosing the `shell` menu item, or go to `list view` and choose the ssh command to use your own terminal. 
 """
 
+## 2025 update: remove VNC and routable_control_ip to make this start more easily on cloudlab
 
 # Import the Portal object.
 import geni.portal as portal
@@ -23,8 +24,9 @@ node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-6
 node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercontent.com/ffund/tcp-ip-essentials/cloudlab/scripts/no-offload.sh | bash'))
 iface0 = node_romeo.addInterface('interface-1', pg.IPv4Address('10.0.1.100','255.255.255.0'))
 node_romeo.exclusive = False
-node_romeo.routable_control_ip = True # required for VNC
-node_romeo.startVNC()
+# no VNC, thus no routable IP required
+#node_romeo.routable_control_ip = True # required for VNC
+#node_romeo.startVNC()
 
 # Node juliet
 node_juliet = request.XenVM('juliet')
@@ -32,8 +34,8 @@ node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-
 node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercontent.com/ffund/tcp-ip-essentials/cloudlab/scripts/no-offload.sh | bash'))
 iface1 = node_juliet.addInterface('interface-3', pg.IPv4Address('10.0.2.100','255.255.255.0'))
 node_juliet.exclusive = False
-node_juliet.routable_control_ip = True # required for VNC
-node_juliet.startVNC()
+#node_juliet.routable_control_ip = True # required for VNC
+#node_juliet.startVNC()
 
 # Node router
 node_router = request.XenVM('router')
@@ -42,8 +44,8 @@ node_router.addService(pg.Execute('/bin/sh','wget -O - https://raw.githubusercon
 iface2 = node_router.addInterface('interface-0', pg.IPv4Address('10.0.1.10','255.255.255.0'))
 iface3 = node_router.addInterface('interface-2', pg.IPv4Address('10.0.2.10','255.255.255.0'))
 node_router.exclusive = False
-node_router.routable_control_ip = True # required for VNC
-node_router.startVNC()
+#node_router.routable_control_ip = True # required for VNC
+#node_router.startVNC()
 
 # Link link-0
 link_0 = request.Link('link-0')
